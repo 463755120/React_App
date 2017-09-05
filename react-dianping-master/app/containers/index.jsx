df84b7b76eb16e00335f4c0b5ed6f26460166a33
王志刚 <463755120@qq.com>
2017-09-05 11:31:07 +0800

    新增dev分支测试

--- react-dianping-master/app/containers/index.jsx
+++ react-dianping-master/app/containers/index.jsx
@@ -29,7 +29,7 @@ class App extends React.Component {
         // 获取位置信息
         let cityName = LocalStore.getItem(CITYNAME)
         if (cityName == null) {
-            cityName = '北京'
+            cityName = '神木'
         }
         this.props.userInfoActions.update({
             cityName: cityName
